22b53e5805887285c85733e55e61f731d76ae9a8
Taylor Bird <nsubordin81@gmail.com>
2024-08-31 17:00:10 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Add routes here
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
