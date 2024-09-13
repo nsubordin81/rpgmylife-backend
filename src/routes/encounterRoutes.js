7c3fe49f19e9ac3aac08da916e33990793fd5e7f
Taylor Bird <nsubordin81@gmail.com>
2024-09-12 23:12:57 -0400

    trying to fix routes

--- src/routes/encounterRoutes.js
+++ src/routes/encounterRoutes.js
@@ -4,8 +4,9 @@ const encounterService = require('../services/encounterService');
 
 const router = express.Router();
 
-router.get('/', encounterService.getEncounters);
-router.post('/', encounterService.createEncounter);
+router.get('/', encounterController.getEncounters);
+router.post('/', encounterController.createEncounter);
 router.put('/:id/complete', encounterController.completeEncounter);
+router.get('/:id', encounterController.getEncounter);
 
 module.exports = router;
